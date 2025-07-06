27d75c49edf64d0626719872230af819846d3ee8
Trung <hotrung1548@gmail.com>
2025-07-06 17:08:43 +0700

    Phân quyền

---
+++ Bảo mật ứng dụng web/spring-security/src/main/java/com/codegym/springsecurity/repository/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.springsecurity.repository;
+
+import com.codegym.springsecurity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
--- Bảo mật ứng dụng web/spring-security/src/main/java/com/codegym/springsecurity/security/SecurityConfiguration.java
+++ Bảo mật ứng dụng web/spring-security/src/main/java/com/codegym/springsecurity/security/SecurityConfiguration.java
@@ -1,33 +1,99 @@
 package com.codegym.springsecurity.security;
 
+import com.codegym.springsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}12345").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}12345").roles("ADMIN");
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/user**").hasRole("USER")
-                .requestMatchers("/admin**").hasRole("ADMIN")
-                .and()
-                .formLogin()
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    @Bean
+    public CustomSuccessHandle customSuccessHandle() {
+        return new CustomSuccessHandle();
     }
+
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder(10));
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        // Disable crsf cho đường dẫn /api/**
+        //http.csrf().ignoringRequestMatchers("/**");
+        http.httpBasic();
+        http
+                .formLogin(formLogin -> formLogin.successHandler(customSuccessHandle())
+                )
+                .formLogin(Customizer.withDefaults());
+        http.authorizeHttpRequests(author -> author
+                        // .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                        // .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                        // .requestMatchers(HttpMethod.GET, "/register").permitAll()
+                        .requestMatchers("/user**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                        .requestMatchers("/admin**").hasRole("ADMIN")
+                        .anyRequest().denyAll()
+                )
+
+                .exceptionHandling(customizer -> customizer.accessDeniedHandler(customAccessDeniedHandler()));
+        //http.csrf(AbstractHttpConfigurer::disable);
+        //http.cors();
+        http
+                // ... other configurations
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout") // Redirect after successful logout
+                        .addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.ALL))) // Clears site data including cookies
+                );
+    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("{noop}12345").roles("USER")
+//                .and()
+//                .withUser("admin").password("{noop}12345").roles("ADMIN");
+//    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                .requestMatchers("/").permitAll()
+//                .requestMatchers("/user**").hasRole("USER")
+//                .requestMatchers("/admin**").hasRole("ADMIN")
+//                .and()
+//                .formLogin()
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+//    }
 }
